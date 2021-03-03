b8846ec51988c1fa5a56263c7b00f0fb5687418c
Dmitriy Matveev <adm.matveeva@yandex.ru>
2021-03-03 19:55:25 +0300

    03

---
+++ jobs.py
@@ -0,0 +1,21 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy import orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user = orm.relation('User')
+
+    def __repr__(self):
+        return f"<User> {self.id} {self.name} {self.email} {self.created_date}"
